6cc826c517db7e30d2465553555432b233fd8cf4
Leo Marquez <lmarqu358@west-mec.org>
2024-12-30 23:34:40 -0700

    made a lot of stuffs

--- short-solstice/src/components/JobInfoPanel.jsx
+++ short-solstice/src/components/JobInfoPanel.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/JobInfoPanel.css";
 
 const JobInfoPanel = ({ job }) => {
+  function approveJob() {
+    alert("Job approved");
+  }
+
+  function rejectJob() {
+    alert("Job rejected");
+  }
   return (
     <div id="job-info-panel">
       <div id="header">
@@ -9,18 +16,30 @@ const JobInfoPanel = ({ job }) => {
       </div>
       <div id="job-details">
         {job ? (
+          // <div className="avenir-book">
           <div>
             <h1>{job.jobName}</h1>
             <div className="job-info-section">
               <h2>Company:</h2>
-              <p>{job.companyName}</p>
+              <h3>{job.companyName}</h3>
+              <p>{job.companyDescription}</p>
+            </div>
+            <div className="job-info-section">
+              <h2>Company Contact Info:</h2>
+              <ul>
+                <li>{job.companyEmail}</li>
+                <li>{job.companyPhone}</li>
+                <li>
+                  <a href={job.website}>{job.website}</a>
+                </li>
+              </ul>
             </div>
             <div className="job-info-section">
               <h2>Job Type:</h2>
               <p>{job.jobType}</p>
             </div>
             <div className="job-info-section">
-              <h3>Salary:</h3>
+              <h2>Salary:</h2>
               <p>
                 ${job.salaryRange.minSalary.toLocaleString()} - $
                 {job.salaryRange.maxSalary.toLocaleString()} a year
@@ -29,6 +48,10 @@ const JobInfoPanel = ({ job }) => {
             <div className="job-info-section">
               <h2>Location:</h2>
               <p>{job.location}</p>
+            </div>
+            <div className="job-info-section">
+              <h2>Work Schedule:</h2>
+              <p>{job.workSchedule}</p>
             </div>
             <div className="job-info-section">
               <h2>Experience Level:</h2>
@@ -59,6 +82,25 @@ const JobInfoPanel = ({ job }) => {
               </ul>
             </div>
             <div className="job-info-section">
+              <h2>Benefits:</h2>
+              <ul>
+                {job.employmentBenefits.map((ben) => (
+                  <li>• {ben}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="job-info-section">
+              <h3>Posted on {job.postedDate}</h3>
+              <h3>Deadline to apply is {job.applicationDeadline}</h3>
+            </div>
+            <div className="job-info-section"></div>
+            <div id="button-wrapper">
+              <button onClick={approveJob} className="button approve">
+                Approve Job Posting
+              </button>
+              <button onClick={rejectJob} className="button reject">
+                Reject Job Posting
+              </button>
             </div>
           </div>
         ) : (
