4ee64683356540f698119af80901541575a76685
Leo Marquez <142362735+LJMarquez@users.noreply.github.com>
2025-01-02 06:35:27 +0000

    navbar works with local storage

--- short-solstice/src/components/JobCarousel.jsx
+++ short-solstice/src/components/JobCarousel.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-// import jobs from "../../dummy-data/jobs.json";
 import "../styles/JobCarousel.css";
 import baseURL from "../baseURL";
 
